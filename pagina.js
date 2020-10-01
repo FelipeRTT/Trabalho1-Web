907e23062c538c247e065ff21db7399d29d92c28
Felipe Ranghetti <feliperanghetti10@gmail.com>
2020-10-01 15:32:27 -0300

    js

---
+++ pagina.js
@@ -0,0 +1,46 @@
+/* Validando os dados do formulario */      
+function validarForm(){
+    var formCorreto;
+    var nome = ancora.nomesobrenome.value;
+    var email = ancora.email.value;
+    var telefone = ancora.telefone.value;
+    var mensagem = ancora.mensagem.value;
+
+    if(nome == ""){
+        alert("Campo nome é obrigatorio");
+        ancora.nomesobrenome.focus();
+        return false;
+    }else{
+        formCorreto = true;
+    }
+
+    if(email == ""){
+        alert("Campo Email é obrigatorio");
+        ancora.email.focus();
+        return false;
+    }else{
+        formCorreto = true;
+    }
+
+    if(telefone == ""){
+        alert("Campo telefone é obrigatorio");
+        ancora.telefone.focus();
+        return false;
+    }else{
+        formCorreto = true;
+    }
+
+    if(mensagem == ""){
+        alert("Campo mensagem é obrigatorio");
+        ancora.mensagem.focus();
+        return false;
+    }else{
+        formCorreto = true;
+    }
+
+    if(formCorreto == true){
+        alert("Formulario enviado com sucesso");
+    }
+}
+
+
